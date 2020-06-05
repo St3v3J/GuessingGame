becb5451855cd0a7c50208d323bedb837ffb458e
Steven Johnson <stevendgjohnson@gmail.com>
2020-06-04 21:51:01 -0500

    Must-have features: edit, delete, list all, details

--- main.py
+++ main.py
@@ -9,72 +9,12 @@ app = Flask(__name__)
 db.create_all()  # create (new) tables in the database
 
 
-@app.route("/profile", methods=["GET"])
-def profile():
-    session_token = request.cookies.get("session_token")
-
-    # get user from the database based on her/his email address
-    user = db.query(User).filter_by(session_token=session_token).first()
-
-    if user:
-        return render_template("profile.html", user=user)
-    else:
-        return redirect(url_for("index"))
-
-
-@app.route("/profile/edit", methods=["GET", "POST"])
-def profile_edit():
-    session_token = request.cookies.get("session_token")
-
-    # get user from the database base on her/his email address
-    user = db.query(User).filter_by(session_token=session_token).first()
-
-    if request.method == "GET":
-        if user:  # if user is found
-            return render_template("profile_edit.html", user=user)
-        else:
-            return redirect(url_for("index"))
-    elif request.method == "POST":
-        name = request.form.get("profile-name")
-        email = request.form.get("profile-email")
-
-        # update the user object
-        user.name = name
-        user.email = email
-
-        # store changes into the database
-        db.add(user)
-        db.commit()
-
-        return redirect(url_for("profile"))
-
-
-@app.route("/profile/delete", methods=["GET", "POST"])
-def profile_delete():
-    session_token = request.cookies.get("session_token")
-
-    # get user from the database based on her/his email address
-    user = db.query(User).filter_by(session_token=session_token).first()
-
-    if request.method == "GET":
-        if user:  # if user is found
-            return render_template("profile_delete.html", user=user)
-        else:
-            return redirect(url_for("index"))
-    elif request.method == "POST":
-        # deltete the user in the database
-        db.delete(user)
-        db.commit()
-
-        return redirect(url_for("index"))
-
-
 @app.route("/", methods=["GET"])
 def index():
     session_token = request.cookies.get("session_token")
 
     if session_token:
-        user = db.query(User).filter_by(session_token=session_token).first()
+        user = db.query(User).filter_by(session_token=session_token, deleted=False).first()
     else:
         user = None
 
@@ -130,7 +70,7 @@ def result():
     session_token = request.cookies.get("session_token")
 
     # get user from the database based on her/his email address
-    user = db.query(User).filter_by(session_token=session_token).first()
+    user = db.query(User).filter_by(session_token=session_token, deleted=False).first()
 
     if guess == user.secret_number:
         message = "Correct! The secret number is {0}".format(str(guess))
@@ -152,16 +92,91 @@ def result():
     return render_template("result.html", message=message)
 
 
+@app.route("/profile", methods=["GET"])
+def profile():
+    session_token = request.cookies.get("session_token")
+
+    # get user from the database based on her/his email address
+    user = db.query(User).filter_by(session_token=session_token, deleted=False).first()
+
+    if user:
+        return render_template("profile.html", user=user)
+    else:
+        return redirect(url_for("index"))
+
+
+@app.route("/profile/edit", methods=["GET", "POST"])
+def profile_edit():
+    session_token = request.cookies.get("session_token")
+
+    # get user from the database based on her/his email address
+    user = db.query(User).filter_by(session_token=session_token, deleted=False).first()
+
+    if request.method == "GET":
+        if user:  # if user is found
+            return render_template("profile_edit.html", user=user)
+        else:
+            return redirect(url_for("index"))
+    elif request.method == "POST":
+        name = request.form.get("profile-name")
+        email = request.form.get("profile-email")
+        old_password = request.form.get("old-password")
+        new_password = request.form.get("new-password")
+
+        if old_password and new_password:
+            hashed_old_password = hashlib.sha256(old_password.encode()).hexdigest()  # hash the old password
+            hashed_new_password = hashlib.sha256(new_password.encode()).hexdigest()  # hash the old password
+
+            # check if old password hash is equal to the password hash in the database
+            if hashed_old_password == user.password:
+                # if yes, save the new password hash in the database
+                user.password = hashed_new_password
+            else:
+                # if not, return error
+                return "Wrong (old) password! Go back and try again."
+
+        # update the user object name and email
+        user.name = name
+        user.email = email
+
+        # store changes into the database
+        db.add(user)
+        db.commit()
+
+        return redirect(url_for("profile"))
+
+
+@app.route("/profile/delete", methods=["GET", "POST"])
+def profile_delete():
+    session_token = request.cookies.get("session_token")
+
+    # get user from the database based on her/his email address
+    user = db.query(User).filter_by(session_token=session_token, deleted=False).first()
+
+    if request.method == "GET":
+        if user:  # if user is found
+            return render_template("profile_delete.html", user=user)
+        else:
+            return redirect(url_for("index"))
+    elif request.method == "POST":
+        # fake delete the user (mark the deleted field as True)
+        user.deleted = True
+        db.add(user)
+        db.commit()
+
+        return redirect(url_for("index"))
+
+
 @app.route("/users", methods=["GET"])
 def all_users():
-    users = db.query(User).all()
+    users = db.query(User).filter_by(deleted=False).all()  # find all un-deleted users
 
     return render_template("users.html", users=users)
 
 
-@app.route("/user/<user_id>", methods=["Get"])
+@app.route("/user/<user_id>", methods=["GET"])
 def user_details(user_id):
-    user = db.query(User).get(int(user_id)) # .get() can help you query by the ID
+    user = db.query(User).get(int(user_id))  # .get() can help you query by the ID
 
     return render_template("user_details.html", user=user)
 
